97ea43793d61530a3ce692a25bffbf5f1f4d163b
Saulo Costa <saulotarsobc@gmail.com>
2022-01-05 19:59:11 -0300

    a

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,25 @@
 const path = require('path');
 
 module.exports = {
-  entry: './assets/scripts/src/index.ts',
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  mode: 'development',
-  output: {
-    filename: 'bundle.js',
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname),
+    entry: './assets/scripts/src/index.ts',
+    resolve: {
+        extensions: ['.ts', '.js'],
     },
-    port: 3000,
-  },
+    module: {
+        rules: [{
+            test: /\.ts$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+        }, ],
+    },
+    mode: 'development',
+    output: {
+        filename: 'bundle.js',
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname),
+        },
+        port: 3000,
+    },
 };
